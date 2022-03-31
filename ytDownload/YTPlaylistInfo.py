7e7f523adb6051176debb68801cac2ca68bb8a7b
FranckNgN <franck5nguyen@gmail.com>
2022-03-31 13:17:40 +0200

    google api-> get all youtube playlists + update on main DL file

---
+++ ytDownload/YTPlaylistInfo.py
@@ -0,0 +1,36 @@
+from ast import Str
+import googleapiclient.discovery
+import pandas as pd
+import os
+
+channel_id = "UC8-FX4KsHFaNy0DQH7BXrdg" # Channel ID set to SenTh
+
+def getYtPlaylistInfoDf(channelId: str) -> pd.DataFrame:
+    cwdPath = os.getcwd() + '\\'
+    dataConfig = pd.read_csv(cwdPath + 'dataConfig.csv') #retrieve youtube API key from excel
+    ytKey = dataConfig['youtubeAPI'][0]
+
+    youtube = googleapiclient.discovery.build("youtube", "v3", developerKey = ytKey)
+
+    request = youtube.playlists().list(
+        part = "snippet",
+        channelId = channelId,
+        maxResults = 50)
+
+    response = request.execute()
+
+    playlists = []
+    while request is not None:
+        response = request.execute()
+        playlists += response["items"]
+        request = youtube.playlists().list_next(request, response)
+
+    playlistsInfoDf = pd.DataFrame(playlists)
+    playlistsInfoDf = playlistsInfoDf.loc[playlistsInfoDf.etag != 'o8vtZVZLSSHZcpfGC3qJ8MGSJ9g'] # delete non anime playlist, #TODO to check why deleted playlist is still showing in the api
+    snippetColumnZip = playlistsInfoDf['snippet'].apply(pd.Series) #Split snippet column
+
+    playlistsInfoDf = pd.concat([playlistsInfoDf, snippetColumnZip], axis = 1) 
+
+    return playlistsInfoDf
+
+getYtPlaylistInfo(channel_id)
